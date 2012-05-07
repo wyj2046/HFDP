de9802c1d00837d2fc670cd1b3206c5f9e4d9481
Wang yujie <wyj2046@gmail.com>
2012-05-07 21:01:19 +0800

    commit for add HeartModel

---
+++ ch12/HeartTestDrive.java
@@ -0,0 +1,10 @@
+public class HeartTestDrive
+{
+  public static void main(String[] args)
+  {
+    HeartModel model = new HeartModel();
+    ControllerInterface controller = new HeartController(model);
+    
+  }
+  
+}
